a81b05ce128528610655766340bd46213dc22ae7
NaStyA128 <n.a.s.t.y.a28v@gmail.com>
2019-04-14 17:48:42 +0300

    One to one fields for Person and Author models.

--- library/users/models.py
+++ library/users/models.py
@@ -3,11 +3,11 @@ from django.db import models
 
 
 class Person(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     additional_information = models.TextField(blank=True, null=True)
 
 
 class Author(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     biography = models.TextField(blank=True, null=True)
     additional_information = models.TextField(blank=True, null=True)
