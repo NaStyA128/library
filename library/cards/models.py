5b62c4d5336d3c904139f53eb46a4d554fc2271a
NaStyA128 <n.a.s.t.y.a28v@gmail.com>
2019-04-14 18:08:48 +0300

    Verbose name for date fields.

--- library/cards/models.py
+++ library/cards/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Card(models.Model):
     person = models.ForeignKey('users.Person', on_delete=models.CASCADE)
     book = models.ForeignKey('books.Book', on_delete=models.CASCADE)
-    date_of_start = models.DateTimeField()
-    date_of_end = models.DateTimeField()
+    date_of_start = models.DateTimeField(verbose_name="Date when user took the book")
+    date_of_end = models.DateTimeField(verbose_name="Date when user have to return the book.")
     is_returned = models.BooleanField(default=False)
     returning_date = models.DateTimeField(null=True)
